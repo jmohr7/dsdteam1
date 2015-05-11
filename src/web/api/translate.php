9f30939de573aa0023941c9b808fc98a948dfc0e
Joe Mohr <jmohr7@uoregon.edu>
2015-05-11 15:21:44 -0700

    Added REST API

--- src/web/api/translate.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once("../translate.php");
-  require_once("../search.php");
-  $translation = translateSentence($_GET("text"), "English", $_GET("targetLanguage"]);
-  $translationLength = strlen($translation);
-  $numSearchResults = getNumSearchResults($translation);
-  shell_exec("java -jar ../../rn.jar ".$_POST['targetLanguage']." ".$translationLength." ".$numSearchResults)
-?>
